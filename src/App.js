11f3b445c098954e256a239410a36994dad5d85d
pakwesi <pakwesiinfornie1@gmail.com>
2020-03-24 15:18:18 +0100

    Added a header to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 
 import Homepage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
+
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -17,8 +19,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+    <Header />
+    <Switch>
     <Route exact path='/' component={Homepage} />
     <Route exact path='/shop' component={ShopPage} />
+    </Switch>
     </div>
   );
 }
